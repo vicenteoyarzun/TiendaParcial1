9b4f3244c9f90e6a990d5be4848a3fd4b665a047
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2025-09-15 20:13:35 -0300

    registro2

--- ProyectoTiendaMascota/registro.js
+++ ProyectoTiendaMascota/registro.js
@@ -2,14 +2,17 @@ const registerForm = document.getElementById("register");
 const errorMessage = document.getElementById("error-message");
 registerForm.addEventListener('submit',function(event){
     event.preventDefault();
+    const email = document.getElementById('email').value;
+    const email2 = document.getElementById('email2').value;
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    if(username!=null && password!=null){
+    const password2 = document.getElementById('password2').value;
+    if(email==email2 && password==password2){
     errorMessage.style.color='green';
     errorMessage.textContent='registro exitoso'
     }else{
     errorMessage.style.color='red';
-    errorMessage.textContent='registro fallido'
+    errorMessage.textContent='correo o contraseña no coinciden'
     }
     registerForm.reset();
 });
