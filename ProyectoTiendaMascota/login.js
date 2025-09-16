2bdcd05fe2abb4e4404d5852337b163c9b359777
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2025-09-15 21:50:06 -0300

    cambio_login

--- ProyectoTiendaMascota/login.js
+++ ProyectoTiendaMascota/login.js
@@ -2,6 +2,8 @@ const loginForm = document.getElementById("login");
 const errorMessage = document.getElementById("error-message");
 const USERCLI = 'Cliente321';
 const PASSCLI = '321123';
+const USERADM = 'admin';
+const PASSADM = 'root';
 loginForm.addEventListener('submit',function(event){
     event.preventDefault();
     const username = document.getElementById('username').value;
@@ -9,6 +11,8 @@ loginForm.addEventListener('submit',function(event){
     if(username === USERCLI && password === PASSCLI){
     errorMessage.style.color='green';
     errorMessage.textContent='Inicio de sesion de usuario exitosa'
+    }else if(username==USERADM && password==PASSADM){
+        window.location.assign("admin.html");
     }else{
     errorMessage.style.color='red';
     errorMessage.textContent='Inicio de sesion de usuario fallida'
